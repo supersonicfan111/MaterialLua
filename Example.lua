6f7e29da8c309d387cffadb5ad9f72a8776ce4d1
supersonicfan111 <rainbowsmokefox@gmail.com>
2020-08-17 16:32:43 +0800

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,4 +1,4 @@
-local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
+local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/supersonicfan111/MaterialLua/master/Module.lua"))()
 
 local X = Material.Load({
 	Title = "ChibuHub",
